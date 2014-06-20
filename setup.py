4cdc8c635b40d34bedbd725aff3adbf99d973edd
Marc Charbit <mcharbit@pentalog.fr>
2014-06-20 16:16:31 +0300

    Created egg distribution

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 __author__ = 'mcharbit'
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='MovieTorrentParser',
     version='0.1.0',
+
+    packages=find_packages(),
+
     author='mcharbit',
     author_email='mcharbit@pentalog.fr',
-    packages=['torrent_parser'],
-    license='LICENSE.txt',
+    license='GPL',
     description='Handy movie torrent parser',
     long_description=open('README.txt').read(),
+
     install_requires=[
-        "lxml >= 2.3.2",
         "IMDbPY >= 5.0",
         "feedparser >= 5.1.3",
     ],
